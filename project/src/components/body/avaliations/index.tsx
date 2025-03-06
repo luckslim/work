4b3fa8907cb163fb3f14b0169911f2fb88166856
lucaslima <lucaslima78@hotmail.com>
2025-03-06 07:29:30 -0300

    up

--- project/src/components/body/avaliations/index.tsx
+++ project/src/components/body/avaliations/index.tsx
@@ -1,69 +1,26 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Container, ContainerAvaible, ContainerButtonComents, ContainerComents, ContainerTestimonial, InfoAvaible } from './style';
-import { StyledSlide } from './style'
-import { Star } from 'phosphor-react';
-import { useState } from 'react';
-import { Collapse } from 'react-bootstrap';
-interface Person {
-    id: string;
-    nome: string;
-    idPerson: string;
-    description: string;
-}
-
-const students: Person[] = [
-    {
-        id: '1',
-        nome: 'Rute Motta Lisboa',
-        idPerson: 'Aluna-OMTC',
-        description: 'A minha jornada na música teve o início na OMTC aos 8 anos de idade, onde aprendi a ler partitura e aprendi minhas primeiras músicas no violino. A OMTC me ajudou no meu desenvolvimento técnico no instrumento e na minha percepção musical. Hoje, tenho 19 anos e estudo música na UNIRIO e tive a oportunidade de tocar em importantes salas de concertos e em Teatros, como a Sala Cecília Meireles e o Teatro Municipal do Rio de Janeiro. ',
-    },
-    {
-        id: '2',
-        nome: 'Sury Silva ',
-        idPerson: 'Aluna-OMTC',
-        description: 'Uma experiência maravilhosa, um ótimo profissional,atencioso e muito didático.',
-    },
-    {
-        id: '3',
-        nome: 'Maicon Libonati',
-        idPerson: 'Aluno-OMTC',
-        description: 'Um excelente lugar para aprender música, ótimas didáticas de ensino. Um estudo de qualidade. ',
-    },
-    {
-        id: '4',
-        nome: 'Pricila Sena',
-        idPerson: 'Aluna-OMTC',
-        description: 'Riquísima em conhecimento,eu recomendo e super indico! Aprendizado de ampla qualidade, Gratidão!',
-    },
-    {
-        id: '5',
-        nome: 'Temotio santos',
-        idPerson: 'Aluno-OMTC',
-        description: 'Estudei por alguns anos e não me arrependo.  A escola de música se destaca pelo profissionalismo e dedicação do professor. Quem se dedicar, com certeza aprenderá muito.',
-    },
-    {
-        id: '6',
-        nome: 'Vanessa Couto',
-        idPerson: 'Responsável-OMTC',
-        description: 'Excelente trabalho desenvolvido pela OMTC. O aluno recebe toda atenção e ensinamentos para se desenvolver no ramo musical. O professor Cláudio está de parabéns!',
-    },
-]
+import {
+    ContainerBody,
+        ContainerSlide,
+        ContainerSlideHeader,
+        ContainerSlideHeaderDescription,
+        SwiperSlideStyled,
+        SwiperStyled,
+         } from './style';
+import pizza from '../../../assets/pizza.png'
+import { AppWindow, CaretUp, Coin, ForkKnife } from 'phosphor-react';
 
 export function TestimonialSwiper(){
-    const [open, setOpen] = useState(false);
     return (
-        <Container>
             <div className="container" data-aos="fade-up" data-aos-delay="100">
-                <Swiper
-                    width={430}
+                <SwiperStyled
                     modules={[Pagination, Autoplay]}
                     loop={true}
                     speed={1000}
-                    autoplay={{ delay: 1000 }}
+                    autoplay={{ delay: 6000 }}
                     slidesPerView="auto"
                     pagination={{
                         el: ".swiper-pagination", // Use a valid DOM element here
@@ -79,69 +36,68 @@ export function TestimonialSwiper(){
                         },
                         1200: {
                             slidesPerView: 1,
-                            spaceBetween: 1,
+                            spaceBetween: 10,
                         },
                     }}
                     className="init-swiper"
                 >
-                    {/* Testimonial 1 */}
-                    {students.map((person) => (
-                        <SwiperSlide key={person.id}>
-
-                            <StyledSlide>
-                                <ContainerTestimonial >
-                                    <ContainerAvaible >
-                                        <img
-
-                                            src=""
-                                            className="testimonial-img"
-                                            alt="Rute Lisboa"
-                                        />
-                                        <span>
-
-                                            <img
-
-                                                src=""
-                                                alt="Google"
-                                            />
-                                        </span>
-
-                                    </ContainerAvaible>
-                                    <InfoAvaible>
-                                        <h4>{person.nome}
-                                            <span>
-                                                <Star size={20} color="#ffee00f9" />
-                                                <Star size={20} color="#ffee00f9" />
-                                                <Star size={20} color="#ffee00f9" />
-                                                <Star size={20} color="#ffee00f9" />
-                                                <Star size={20} color="#ffee00f9" />
-                                            </span>
-                                        </h4>
-                                        <ContainerButtonComents>
-                                            <small>{person.idPerson}</small>
-                                            <a                 
-                                                onClick={() => setOpen(!open)}
-                                                aria-controls="example-collapse-text"
-                                                aria-expanded={open}>
-                                                {open ? 'Fechar' : 'Abrir'}
-                                            </a>
-                                        </ContainerButtonComents>
-                                    </InfoAvaible>
-                                    <ContainerComents>
-                                        <Collapse in={open}>
-                                            <p>{person.description}</p>
-                                        </Collapse>
-                                    </ContainerComents>
-                                </ContainerTestimonial>
-                            </StyledSlide>
-
-
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                    
+                   
+                        <SwiperSlideStyled>
+                            <ContainerSlide>
+                                <ContainerSlideHeader>
+                                    <img src={pizza} alt="" />
+                                    <div>
+                                        <p><ForkKnife size={32} /></p>
+                                        <p>Peça seu App</p>
+                                    </div>
+                                </ContainerSlideHeader>
+                                <ContainerBody>
+                                    <h1>Aplicativo para Restaurantes</h1>
+                                    <div>
+                                        <ul>
+                                            <li>Facilidade no atendimento</li>
+                                            <li>Melhor gestão de pedidos</li>
+                                            <li>Maior engajamento</li>
+                                        </ul>
+                                        <p>Um aplicativo para restaurantes permite uma experiência mais fluida e moderna para os clientes</p>
+                                    </div>
+                                    <button>Peça Seu App</button>
+                                </ContainerBody>
+                            </ContainerSlide>
+                        </SwiperSlideStyled>
+                        <SwiperSlideStyled>
+                            <ContainerSlide>
+                                <ContainerSlideHeaderDescription>
+                                    <div>
+                                    <CaretUp color="#5fcf8e" size={32} weight="fill" />
+                                    <p>Aumente Suas Vendas</p>
+                                    <Coin size={22} color="#31758a"/>
+                                    <p>Cabe no seu bolso</p>
+                                    </div>
+                                    <div>
+                                        <p><AppWindow size={22} /></p>
+                                        <p>Peça seu App</p>
+                                    </div>
+                                </ContainerSlideHeaderDescription>
+                                <ContainerBody>
+                                    <h1>Sites Profissionais e Pessoais</h1>
+                                    <div>
+                                        <ul>
+                                            <li>Integração com Banco de dados.</li>
+                                            <li>Site responsivo para todas as telas.</li>
+                                            <li>Lojas Virtuais.</li>
+                                        </ul>
+                                        <p>Transforme seu negócio com tecnologia e agregue valor com inovação digital.</p>
+                                    </div>
+                                    <button>Peça Seu App</button>
+                                </ContainerBody>
+                            </ContainerSlide>
+                        </SwiperSlideStyled>
+                </SwiperStyled>
                 <div className="swiper-pagination"></div>
             </div>
-        </Container>
+
 
 
     );
--- project/src/components/body/avaliations/style.ts
+++ project/src/components/body/avaliations/style.ts
@@ -1,76 +1,150 @@
 import styled from "styled-components";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export const Container =styled.div`
-    margin: 100px 0px ;
-    width: 430px;
+export const SwiperStyled = styled(Swiper)`
+    height: 500px;
     
 `;
 
-export const StyledSlide = styled.div`
-    background-color: ${props=>props.theme['gray-700']};
-    border-radius: 8px;
-    padding: 2rem;
-    height: auto;
+export const SwiperSlideStyled = styled(SwiperSlide)`
+    height: 500px;
 `;
-export const ContainerTestimonial = styled.div`
+export const ContainerSlide = styled.div`
+    display: grid;
+`;
+export const ContainerSlideHeader = styled.div`
+    display: flex;
+    align-items: end;
+    justify-content: space-around;
+    margin-top: 25px;
+    div{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;    
+    }
+    img{
+        width: 150px;
+        animation: rotate 20s linear infinite;
+
+    }
+    @keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+
+`;
+export const ContainerBody=styled.div`
     display: flex;
     flex-direction: column;
-`;
-export const ContainerAvaible = styled.div`
-    display: flex;
-    justify-content: space-between;
     align-items: center;
-    
-    img{
-        width: 88px;
-        height: 88px;
-        object-fit: cover;
-        border-radius: 9999px;
+    h1{
+        font-size: 25px;
+        color:${props=>props.theme['slate-700']};
+        font-weight: 800;
     }
-    span img{
-        width: 40px;
-        object-fit: contain;
-    }
-`;
-export const InfoAvaible = styled.div`
-    line-height: 40px;
-    h4{
-        border-bottom: 1px solid ${props=>props.theme['violet-500']};
-        color: ${props=>props.theme['gray-100']};
+    div{
         display: flex;
         justify-content: space-between;
-        margin: 10px 0 ;
+        p{
+            width: 300px;
+            border-left: 1px solid ${props=>props.theme['slate-400']};
+            padding: 1rem;
+        }
+
     }
-    small{
-        color: ${props=>props.theme['gray-100']};
-    }
-`;
-
-export const ContainerComents = styled.div`
-
-`;
-
-export const ContainerButtonComents = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    justify-items: center;
-    color: aliceblue;
-    a{
-        text-decoration: none;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: ${props=>props.theme['gray-800']};
-        padding: 0.1rem;
-        width: 100px;
-
+    button{
+        background-color: ${props=>props.theme['slate-950']};
+        width: 300px;
+        color:${props=>props.theme['slate-100']};
+        padding: 1rem;
+        border: none;
         border-radius: 8px;
-        height: 30px;
-        cursor: pointer;
-
     }
-    a:hover{
-
+    button:hover{
+        opacity: 70%;
+        transition: opacity 1s;
     }
 `;
+export const ContainerSlideHeaderDescription = styled.div`
+    display: flex;
+    justify-content: space-around;
+    margin-top: 80px;
+    div{
+        text-align: center;
+    }
+`;
+
+// export const StyledSlide = styled.div`
+//     background-color: ${props=>props.theme['gray-700']};
+//     border-radius: 8px;
+//     padding: 2rem;
+//     height: auto;
+// `;
+// export const ContainerTestimonial = styled.div`
+//     display: flex;
+//     flex-direction: column;
+// `;
+// export const ContainerAvaible = styled.div`
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+    
+//     img{
+//         width: 88px;
+//         height: 88px;
+//         object-fit: cover;
+//         border-radius: 9999px;
+//     }
+//     span img{
+//         width: 40px;
+//         object-fit: contain;
+//     }
+// `;
+// export const InfoAvaible = styled.div`
+//     line-height: 40px;
+//     h4{
+//         border-bottom: 1px solid ${props=>props.theme['violet-500']};
+//         color: ${props=>props.theme['gray-100']};
+//         display: flex;
+//         justify-content: space-between;
+//         margin: 10px 0 ;
+//     }
+//     small{
+//         color: ${props=>props.theme['gray-100']};
+//     }
+// `;
+
+// export const ContainerComents = styled.div`
+
+// `;
+
+// export const ContainerButtonComents = styled.div`
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     justify-items: center;
+//     color: aliceblue;
+//     a{
+//         text-decoration: none;
+//         display: flex;
+//         align-items: center;
+//         justify-content: center;
+//         background-color: ${props=>props.theme['gray-800']};
+//         padding: 0.1rem;
+//         width: 100px;
+
+//         border-radius: 8px;
+//         height: 30px;
+//         cursor: pointer;
+
+//     }
+//     a:hover{
+
+//     }
+// `;
